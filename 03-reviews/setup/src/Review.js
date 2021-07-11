392963515aa23c68e59c904c942c5c3fb0c0d032
Doug Barr <douglas.barr01@gmail.com>
2021-07-11 13:23:23 +0100

    random number functionality

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -30,7 +30,13 @@ const Review = () => {
     });
    }
 
-
+   const randomPerson = () => {
+     let randomNumber = Math.floor(Math.random() * people.length);
+     if(randomNumber === index) {
+       randomNumber = index + 1
+     }
+     setIndex(checkNumber(randomNumber))
+   }
 
 
 
@@ -53,7 +59,7 @@ const Review = () => {
           <FaChevronRight />
          </button>
        </div>
-         <button className="random-btn">surprise me</button>
+         <button className="random-btn" onClick={randomPerson}>surprise me</button>
      </article>
    );
 };
