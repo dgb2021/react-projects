1c9c9c09592f4b448c0fbe20d8fc7ae79d14164b
Doug Barr <douglas.barr01@gmail.com>
2021-07-13 14:26:16 +0100

    __

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -6,7 +6,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello world')
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1
+    }
+    if (count > 8) {
+      amount = 8
+    }
+    setText(data.slice(0, amount));
   }
 
  return (
@@ -21,6 +28,12 @@ function App() {
        />
        <button type="submit" className="btn">generate</button>
      </form>
+     <article>
+     {text.map((item, index) => {
+       return <p key={index}>{item}</p>
+     })}
+
+     </article>
    </section>
  )
 }
