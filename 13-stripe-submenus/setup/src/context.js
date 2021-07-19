0d150fe6bebb3e5043587e71895af90b6db1a258
Doug Barr <douglas.barr01@gmail.com>
2021-07-19 15:00:03 +0100

    AppContext and globalcontext

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,32 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({children}) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true)
+  }
+  const openSidebar = () => {
+    setIsSidebarOpen(false)
+  }
+  const openSubmenu = () => {
+    setIsSubmenuOpen(false)
+  }
+ const closeSubmenu = () => {
+    setIsSubmenuClose(false)
+  }
+
+return <AppContext.Provider value={{
+  isSubmenuOpen, isSidebarOpen, openSubmenu, openSidebar, closeSubmenu, closeSidebar
+}}>
+  {children}
+</AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
