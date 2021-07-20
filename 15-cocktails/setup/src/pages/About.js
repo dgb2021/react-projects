382ecb59ef3dcde04a6cb6b76face72ba32fcd9a
Doug Barr <douglas.barr01@gmail.com>
2021-07-20 15:46:49 +0100

    basic pages up and running

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis molestie quis ipsum euismod tincidunt. Nunc pellentesque sodales fringilla. Phasellus auctor, tortor ornare malesuada mattis, diam ex cursus tortor, et sodales ante urna a nunc. Aliquam vel porttitor enim. Morbi vel magna ex.</p1>
+
+
+    </section>
   )
 }
 
