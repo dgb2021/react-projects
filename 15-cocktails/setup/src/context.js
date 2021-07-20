ebb4ee578b4dd6c2658569f7fa9323943000f651
Doug Barr <douglas.barr01@gmail.com>
2021-07-20 18:33:01 +0100

    useCallback

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -9,7 +9,7 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('a')
   const [cocktails, setCocktails] = useState([])
 
-  const fetchDrinks = async () => {
+  const fetchDrinks = useCallback(async () => {
     setLoading(true)
     try {
       const response = await fetch(`${url}${searchTerm}`)
@@ -41,11 +41,11 @@ const AppProvider = ({ children }) => {
      
       setLoading(false)
     }
-  }
+  }, [searchTerm])
 
   useEffect(() => {
     fetchDrinks()
-  }, [searchTerm])
+  }, [searchTerm, fetchDrinks])
 
 
 
